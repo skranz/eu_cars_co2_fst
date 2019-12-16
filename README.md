07df950675cce41f3b0ff4ca4f68e7eac248be81
skranz <sebastian.kranz@uni-ulm.de>
2019-12-16 08:50:27 +0000

    change README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+This repositorium contains the EU data sets on all newly registered cars (including their emmission data) in format usable by the R package [fst](https://www.fstpackage.org/).
+
+The original data sets are provided here:
+
+[https://www.eea.europa.eu/data-and-maps/data/co2-cars-emission-16](https://www.eea.europa.eu/data-and-maps/data/co2-cars-emission-16)
+
+I provided this version in fst format because handling the large CSV files that are partially UTF-16 encoded may be daunting on some computers. The file `download_and_convert.R` provides the download and conversion code.
+
+The files for 2018 are split in 5 roughly equal parts because Github imposes a maximum file size of 100 MB.
---
+++ download_and_convert.R
@@ -0,0 +1,51 @@
+# Download ZIP with CSV
+# from https://www.eea.europa.eu/data-and-maps/data/co2-cars-emission-16
+# and save as fst file.
+
+run = function() {
+  # Example 2018
+  year = 2018
+  url ="http://ftp.eea.europa.eu/www/co2/CO2_passenger_cars_v17_csv.zip"
+  download.and.convert(year, url)  
+}
+
+
+download.and.convert = function(year, url) {
+  library(fst)
+  library(rio)
+  library(dplyr)
+  
+  fst.file = paste0("cars",year,".fst")
+  zip.file = paste0("cars",year,".zip")
+  if (file.exists(fst.file)) next
+  
+  url = urls$url[i]
+  
+  if (!file.exists(zip.file))
+    download.file(url, zip.file)
+  
+  # Note R unzip does not work for 2018 data
+  # unzip(zip.file)
+
+  # Use linux unzip instead  
+  cmd = paste0("unzip ", zip.file)
+  system(cmd)
+  csv.file = "CO2_passenger_cars.csv"
+  #csv.file = list.files(pattern=glob2rx("*.csv"))[1]
+  
+  dat = try(rio::import(csv.file))
+  
+  # Some files use UTF-16 encoding. Use good old read.csv to deal with it
+  if (is(dat, "try-error")) {
+    dat = read.csv(csv.file,fileEncoding = "UTF-16", sep="\t")
+  }
+  
+  # Write fst file with maximum compression
+  write_fst(dat, fst.file,compress = 100)
+  
+  # Remove large csv.file
+  file.remove(csv.file) 
+}
+
+
+
